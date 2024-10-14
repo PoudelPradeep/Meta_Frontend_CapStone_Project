ae975d63732e097366dcc6e7e1f071f75ce13ec7
PoudelPradeep <pradeeppoudeledu@gmail.com>
2024-10-14 00:51:51 -0500

    divided into module for React Application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+import Main from '../components/Main';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <React.Fragment>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main(){
+    return (
+        <main>
+            <section>
+                <h2>About Our Services</h2>
+                <p>We provide top-notch services to meet your needs.</p>
+            </section>
+        </main>
+    )
+}
+
+export default Main;
