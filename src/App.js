240ae9d055ee5a7d5c983c93395e323ffdd7dcb3
PoudelPradeep <pradeeppoudeledu@gmail.com>
2024-10-14 00:55:21 -0500

    updated the path for components in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Nav from '../components/Nav';
-import Main from '../components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
